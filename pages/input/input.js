bd206bbfe35acecc79b3ad235c7912cef8c247e5
CoderQiQin521 <coderqiqin@aliyun.com>
2019-04-25 17:37:48 +0800

    还原

--- pages/input/input.js
+++ pages/input/input.js
@@ -1,25 +1,25 @@
 // pages/input/input.js
-let http = require('../../http/request')
-let api = require('../../http/api')
-let utils = require('../../utils/util')
-let config = require('../../utils/config')
+let http          = require('../../http/request')
+let api           = require('../../http/api')
+let utils         = require('../../utils/util')
+let config        = require('../../utils/config')
 let cryptoService = require('../../utils/cryptoService')
-let pay = require('../../utils/pay')
+let pay           = require('../../utils/pay')
 
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    code: '',
-    bindData: {},
-    openKey: [],
-    focus: true,
-    isInit: false,    //是否初始化
-    isConnect: false, //是否连接上
-    yjtoken: [],
-    sys: '', // 手机系统,区分ios和andriod
-    firm: 0 // 设备厂家 1.asdd  2.yj
+    code     : '',
+    bindData : {},
+    openKey  : [],
+    focus    : true,
+    isInit   : false,   //是否初始化
+    isConnect: false,   //是否连接上
+    yjtoken  : [],
+    sys      : '',      // 手机系统,区分ios和andriod
+    firm     : 0        // 设备厂家 1.asdd  2.yj
   },
   onChange(e) {
     this.setData({ code: e.detail });
@@ -36,13 +36,14 @@ Page({
       }, (err) => { // 初始化失败
         console.log(err);
         wx.showLoading({
-          title: '请开蓝牙',
-          icon: 'loading',
+          title   : '请开蓝牙',
+          icon    : 'loading',
           duration: 2000
         })
       });
     }
   },
+  // 开锁业务逻辑
   httpcon() {
     let that = this,
         code = this.data.code;
@@ -59,18 +60,18 @@ Page({
                 let bindData = res.data;
                 console.log('bindData: ', bindData);
                 that.setData({ bindData, firm: res.data.id })
-                that.openLock();
+                that.diffSys();
               }else {
                 wx.showToast({
                   title: '没有该设备',
-                  icon: 'none'
+                  icon : 'none'
                 })
               }
             })
           }else if(res.error_code === 1) {
             wx.showToast({
               title: res.msg,
-              icon: 'none'
+              icon : 'none'
             })
           }
         })
@@ -88,13 +89,13 @@ Page({
                   if (res.error_code !== 0) {
                     wx.showToast({
                       title: res.msg,
-                      icon: 'none'
+                      icon : 'none'
                     })
                     return;
                   }
                   let bindData = res.data;
                   that.setData({ bindData, firm: res.data.id })
-                  that.openLock();
+                  that.diffSys();
                 })
               }else if (res.error_code === 1) {
                 // 微信支付 pay/pays_order?did=4
@@ -106,11 +107,11 @@ Page({
                       if(res.error_code === 0) {
                         let bindData = res.data;
                         that.setData({ bindData, firm: res.data.id })
-                        that.openLock();
+                        that.diffSys();
                       }else {
                         wx.showToast({
                           title: res.msg,
-                          icon: 'none'
+                          icon : 'none'
                         })
                         return
                       }
@@ -122,14 +123,14 @@ Page({
           }else if(res.error_code === 1) {
             wx.showToast({
               title: res.msg,
-              icon: 'none'
+              icon : 'none'
             })
           }
         })
       }else if(res.error_code === 503) {
         wx.showToast({
           title: res.msg,
-          icon: 'none'
+          icon : 'none'
         })
         setTimeout(() => {
           wx.navigateTo({
@@ -144,24 +145,19 @@ Page({
     // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
     if (!wx.openBluetoothAdapter) {
       wx.showModal({
-        title: '提示',
+        title  : '提示',
         content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
       })
-      return
+      return;
     }
     return new Promise(function(reject, resolve) {
       wx.openBluetoothAdapter({
         success(res) {
-          reject(res)
+          reject(res);
           // that.getBluetoothAdapterState();
         },
         fail(err) {
-          resolve(err)
-          // wx.showToast({
-          //   title: '请开启手机蓝牙再试',
-          //   icon: 'none'
-          // })
-          // return
+          resolve(err);
         }
       })
     })
@@ -171,12 +167,11 @@ Page({
     let that = this;
     wx.getBluetoothAdapterState({
       success(res) {
-        let { available, discovering } = res
-
+        let { available, discovering } = res;
         if(!available) {
           wx.showToast({
             title: '请开启手机蓝牙再试',
-            icon: 'none'
+            icon : 'none'
           })
           return;
         }
@@ -190,60 +185,57 @@ Page({
       fail(err) {
         console.log(err);
       }
-    })
+    });
   },
-  // 开锁
-  openLock() {
-    let sys = this.data.sys,
-    isInit = this.data.isInit,
-    isConnect = this.data.isConnect,
-    yjtoken = this.data.yjtoken;
-    console.log('开锁中.....');
+  // 区分手机系统
+  diffSys() {
+    let sys = this.data.sys;
     /*
-    ios和安卓的区别在连接后要不要再次获取设备id 和 服务uuid
+      ios和安卓的区别
+      安卓不需要匹配直接连接,ios需要先蓝牙匹配
+      在连接后要不要再次获取设备id 和 服务uuid
     */
-    
-
-    if(isConnect) {
-      that.writeBLECharacteristicValue(yjtoken);
-    }else {
-      if (sys === 'ios') {
-        // this.iosPhone();
-        this.startBluetoothDevicesDiscovery();
-      }else if(sys === 'android') {
-        this.androidPhone()
-      }
+    if (sys === 'ios') {
+      this.iosPhone();
+    }else if(sys === 'android') {
+      this.androidPhone();
     }
-    
-    // 检查是否有已连接设备
-    // wx.getConnectedBluetoothDevices({
-    //   success: function (res) {
-    //     ;
-    //     if(res.devices.length) {
-    //       that.writeBLECharacteristicValue(openKey)
-    //     }else {
-    //       that.connectTO(bindData.deviceId, bindData.serviceId, bindData.readId)
-    //     }
-    //   }
-    // })
+  },
+  iosPhone() {
+    /*
+      ios
+      需要搜索匹配
+      需要解析id
+    */
+    this.startBluetoothDevicesDiscovery();
+  },
+  // 安卓手机
+  androidPhone() {
+    let bindData  = this.data.bindData,
+        isConnect = this.data.isConnect;
+    if(isConnect) {
+        this.notifyBLECharacteristicValueChange(bindData.deviceId, bindData.serviceId, bindData.readId);
+    }else {
+      this.connectTO(bindData.deviceId, bindData.serviceId, bindData.readId);
+    }
   },
   startBluetoothDevicesDiscovery() {
     let that = this;
     wx.startBluetoothDevicesDiscovery({
-      services: ['FFF0', 'FEE7'], // 过滤其他设备
-      success: function (res) {
-        that.onBluetoothDeviceFound()
+      services: ['FFF0', 'FEE7'],   // 过滤其他设备
+      success : function (res) {
+        that.onBluetoothDeviceFound();
       }
     })
   },
   // 监听寻找到新设备的事件
   onBluetoothDeviceFound() {
-    let that = this,
+    let that     = this,
         bindData = this.data.bindData,
-        firm = this.data.firm;
+        firm     = this.data.firm;
 
     wx.onBluetoothDeviceFound(function(res) {
-      let advertisData = null; //ios下通过设备advertisData获取设备id
+      let advertisData = null;  //ios下通过设备advertisData获取设备id
 
       res.devices.forEach(item => {
         if(firm === 1) {
@@ -273,42 +265,12 @@ Page({
   },
   // 获取连接设备的service服务uuid
   getBLEDeviceServices: function (deviceId) {
-    let that = this,
-        firm = this.data.firm,
-        bindData = this.data.bindData;
+    let that = this;
 
     wx.getBLEDeviceServices({
       deviceId: deviceId,
-      success: function (res) {
-        console.log('res: ', res);
+      success : function (res) {
         let serviceId = res.services[0].uuid;
-        // bindData.deviceId = deviceId
-        // bindData.serviceId = serviceId
-        // that.setData({
-        //   bindData
-        // });
-        
-        // that.getBLEDeviceCharacteristics(deviceId, serviceId)
-        // return
-
-
-        //TODO: 貌似不用获取特征码,可以直接开锁
-        // if(firm === 1) { // 判断厂商设备
-        //   that.getBLEDeviceCharacteristics(deviceId, serviceId);
-        //   // return
-        //   // that.notifyBLECharacteristicValueChange(deviceId, serviceId, bindData.writeId);
-        //   // ;
-        //   // ;
-        //   // ;
-          
-        // }else if(firm === 2) { // 宜家
-        //   that.getBLEDeviceCharacteristics(deviceId, serviceId);
-        //   // that.notifyBLECharacteristicValueChange(deviceId, serviceId, bindData.readId);
-        //   // ;
-        //   // ;
-        //   // ;
-        //   // ;
-        // }
 
         that.getBLEDeviceCharacteristics(deviceId, serviceId);
       }
@@ -324,7 +286,7 @@ Page({
       deviceId: deviceId,
       // 这里的 serviceId 需要在上面的 getBLEDeviceServices 接口中获取
       serviceId: serviceId,
-      success: function (res) {
+      success  : function (res) {
         if(firm === 1) {
           that.notifyBLECharacteristicValueChange(deviceId, serviceId, res.characteristics[0].uuid)
         }else if(firm === 2) {
@@ -333,38 +295,16 @@ Page({
       }
     })
   },
-  iosPhone() {
-    this.startBluetoothDevicesDiscovery()
-  },
-  // 安卓手机
-  androidPhone() {
-    let bindData = this.data.bindData,
-    isConnect = this.data.isConnect;
-    console.log('isConnect: ', isConnect);
-    if(isConnect) {
-      // that.notifyBLECharacteristicValueChange(bindData.deviceId, bindData.serviceId, bindData.readId);
-
-      // let yjtoken = this.data.yjtoken;
-      //   console.log('yjtoken: ', yjtoken);
-        
-      //   this.writeBLECharacteristicValue(yjtoken);
-
-        this.notifyBLECharacteristicValueChange(bindData.deviceId, bindData.serviceId, bindData.readId)
-    }else {
-      this.connectTO(bindData.deviceId, bindData.serviceId, bindData.readId);
-    }
-    
-  },
-  // 向低功耗蓝牙设备特征值中写入二进制数据(宜家)
-  yijiaLock(sData) {
+  // (宜家加密数据)
+  yijiaProcessing(sData) {
     /*
       宜家锁收发数据js中为16位数据,可以为字符串,不影响结果
-      向蓝牙锁写入分2步:
+      向蓝牙锁写入分2步: 
       1. 处理指令
       2. 加密
     */
     let sendData = null,
-        len = 16 - sData.length;
+        len      = 16 - sData.length;
     // 1处理指令
     for(let i = 0; i < len; i++) {
       sData.push('0x30')
@@ -376,24 +316,24 @@ Page({
 
     // 转换为ArrayBuffer
     let enDataBuf = new Uint8Array(bData);
-    sendData = enDataBuf.buffer;
+        sendData  = enDataBuf.buffer;
     return sendData;// 返回处理数据
   },
   getYjToken() {
     let bindData = this.data.bindData,
-    sendData = null,
-    that = this,
-    noToken = false;
+        sendData = null,
+        that     = this,
+        yjtoken  = this.data.yjtoken;
 
     // 先开启写入端口
     wx.notifyBLECharacteristicValueChange({
-      state: true,
-      deviceId: bindData.deviceId,
-      serviceId: bindData.serviceId,
+      state           : true,
+      deviceId        : bindData.deviceId,
+      serviceId       : bindData.serviceId,
       characteristicId: bindData.readId,
       success(res) {
         // 获取Token
-        sendData = that.yijiaLock(config.DIRECTIVE.yijia.token);
+        sendData = that.yijiaProcessing(config.DIRECTIVE.yijia.token);
         console.log('sendData: ', sendData);
 
         wx.onBLECharacteristicValueChange(function (characteristic){
@@ -406,21 +346,20 @@ Page({
               return '0x' + item.toString(16).toUpperCase()
             });
             var token = [lockData[3], lockData[4], lockData[5],lockData[6]];
-            console.log('token: ', token)
-            if(!noToken) {
-              that.setData({ yjtoken: token })
-              noToken = true;
+            if(!yjtoken.length) {
+              that.setData({ yjtoken: token });
             }
           }
         });
 
         wx.writeBLECharacteristicValue({
-          deviceId: bindData.deviceId,
-          serviceId: bindData.serviceId,
+          deviceId        : bindData.deviceId,
+          serviceId       : bindData.serviceId,
           characteristicId: bindData.writeId,
-          value: sendData,
+          value           : sendData,
           success(res) {
             console.log('token获取成功',res);
+            that.openYj();
           },
           fail(err) {
             console.log('token获取失败',err);
@@ -431,13 +370,13 @@ Page({
         console.log(err);
         wx.showToast({
           title: 'notify接口开启失败',
-          icon: 'none'
+          icon : 'none'
         })
       }
     })
   },
   openYj() {
-    let pass = ['0x30', '0x30', '0x30', '0x30', '0x30', '0x30'],
+    let pass  = ['0x30', '0x30', '0x30', '0x30', '0x30', '0x30'],
         token = this.data.yjtoken;
         console.log('存储token: ', token);
     
@@ -448,29 +387,29 @@ Page({
         }else {
           wx.showToast({
             title: '请先获取token',
-            icon: 'none'
+            icon : 'none'
           })
         }
   },
   writeBLECharacteristicValue(sData, btRandrom) {
     console.log('执行writeBLECharacteristicValue');
     
-    let firm = this.data.firm,
+    let firm     = this.data.firm,
         bindData = this.data.bindData,
         sendData = null,
-        that = this;
+        that     = this;
     if(firm === 1) {
       sendData = this.asddLock(sData, btRandrom)
     }else {
-      sendData = this.yijiaLock(sData)
+      sendData = this.yijiaProcessing(sData)
     }
 
     //TODO: 封装写入函数
     wx.writeBLECharacteristicValue({
-      deviceId: bindData.deviceId,
-      serviceId: bindData.serviceId,
+      deviceId        : bindData.deviceId,
+      serviceId       : bindData.serviceId,
       characteristicId: bindData.writeId,
-      value: sendData,
+      value           : sendData,
       success(res) {
         console.log('写入成功',res);
         that.setData({ isConnect: true })
@@ -487,11 +426,11 @@ Page({
   // 连接设备
   connectTO: function (deviceId, serviceId, characteristicId) {
     let that = this,
-        sys = this.data.sys;
+        sys  = this.data.sys;
         console.log('deviceId: ', deviceId);
     wx.createBLEConnection({
       deviceId: deviceId,
-      success: function (res) {
+      success : function (res) {
         that.setData({
           isConnect: true
         })
@@ -500,13 +439,13 @@ Page({
           that.getBLEDeviceServices(deviceId)
         }else if(sys === 'android') {
           // // 获取服务id
-          // that.notifyBLECharacteristicValueChange(deviceId, serviceId, characteristicId)
+          that.notifyBLECharacteristicValueChange(deviceId, serviceId, characteristicId)
         }
       },
       fail: function (err) {
         wx.showToast({
           title: '连接设备失败',
-          icon: 'none'
+          icon : 'none'
         })
       },
       complete: function () {}
@@ -515,24 +454,24 @@ Page({
   // 启用低功耗蓝牙设备特征值变化时的 notify 功能
   notifyBLECharacteristicValueChange(deviceId, serviceId, characteristicId) {
     let that = this,
-        firm = this.data.firm;//厂家
+        firm = this.data.firm;  //厂家
 
     if(firm === 1) {
       wx.notifyBLECharacteristicValueChange({
-        state: true,
-        deviceId: deviceId,
-        serviceId: serviceId,
+        state           : true,
+        deviceId        : deviceId,
+        serviceId       : serviceId,
         characteristicId: characteristicId,
         complete(res) {
           wx.onBLECharacteristicValueChange(function (characteristic) {
             if (characteristic.characteristicId.toLowerCase().indexOf("fff6") != -1) {
               var char6value = new Uint8Array(characteristic.value, 0);
-              var tmp = "";
-              tmp = String.fromCharCode.apply(String, char6value);
+              var tmp        = "";
+                  tmp        = String.fromCharCode.apply(String, char6value);
   
               if (tmp.substring(0, 3) == 'KEY') 
               { 
-                let btRandrom = char6value.subarray(3); 
+                let btRandrom = char6value.subarray(3);
                 that.writeBLECharacteristicValue(config.DIRECTIVE.asdd.open, btRandrom);
               }else {
                 if (tmp.toUpperCase() == "UNLOCK") {
@@ -550,20 +489,19 @@ Page({
         fail(err){
           wx.showToast({
             title: 'notify接口开启失败',
-            icon: 'none'
+            icon : 'none'
           })
         }
       })
     }else if(firm === 2) {// 宜家
-      
+      that.getYjToken();
     }
   },
   /* 
     asdd厂家开锁逻辑
   */
  asddLock(sData, btRandrom) {
-  let sendData = null,
-      bindData = this.data.bindData;
+  let sendData = null;
 
   if (btRandrom) {
     let cryptoKey = new Uint8Array(config.KEY_ASDD);
@@ -571,9 +509,9 @@ Page({
       cryptoKey[i] = btRandrom[i];
     }
 
-    let bData = cryptoService.encrypt(sData, cryptoKey);
+    let bData     = cryptoService.encrypt(sData, cryptoKey);
     let enDataBuf = new Uint8Array(bData);
-    sendData = enDataBuf.buffer;
+        sendData  = enDataBuf.buffer;
   }
   return sendData;
 },
@@ -584,7 +522,7 @@ Page({
     let code = e.code;
     if(code) {
       this.setData({
-        code: code,
+        code : code,
         focus: false
       })
     }
